1ef9c0f05eb92eb040ea832946c6002f2bbca9c3
MarcusJones <jones.0bj3@gmail.com>
2019-01-26 13:00:20 +0100

    refactor Final data table documented

--- src/1_data/data_01.py
+++ src/1_data/data_01.py
@@ -5,68 +5,105 @@
 # %% [markdown]
 # # Currency and social columns
 # %%
-currency_and_social_columns = df_sales.columns[9:].tolist()
-print(currency_and_social_columns)
-logging.info("{} currency_and_social_columns columns".format(len( currency_and_social_columns )))
+# Sales data breakdown:
+#
+# Currency:
+# 5 Currency rates: USD, GBP, CNY, JPY, KRW x 8 days = 40
+#
+# Social:
+# TotalBuzzPost
+# TotalBuzz
+# PositiveSentiment
+# NegativeSentiment
+# NetSentiment
+# Impressions
+# TOTAL = 6
+# And for each of the 7 days = 42
+# 40 + 48 = 88 columns
+def log_df(df,name):
+    logging.info("DataFrame {} {}".format(name,df.shape))
+log_df(df_all,'df_all')
 
+#%%
+logging.info("SALES DATA".format())
+
+cols_currency_and_social = df_sales.columns[9:].tolist()
+print(cols_currency_and_social)
+logging.info("{} currency_and_social_columns columns".format(len(cols_currency_and_social)))
+
+# %% [markdown]
+# This is a dataframe of ONLY the first day of activity
 # %%
-first_day = df_sales.loc[df_sales.Date == 'Day_1', :]
+df_first_day = df_sales.loc[df_sales.Date == 'Day_1', :]
+log_df(df_first_day,'df_first_day')
+df_second_day = df_sales.loc[df_sales.Date == 'Day_2', :]
+log_df(df_first_day,'df_second_day')
 # print(first_day)
 
 # %%
-
+# Get the mean of ALL columns for ALL sku
+df_all_currency_and_social = df_sales.groupby('sku_hash').mean()[cols_currency_and_social]
 #
-all_currency_and_social = df_sales.groupby('sku_hash').mean()[currency_and_social_columns]
-first_day_currency_and_social = first_day.groupby('sku_hash').mean()[currency_and_social_columns]
-first_day_currency_and_social.columns = ['first_day_' + col for col in first_day_currency_and_social.columns]
+df_first_day_currency_and_social = df_first_day.groupby('sku_hash').mean()[cols_currency_and_social]
+df_first_day_currency_and_social.columns = ['first_day_' + col for col in df_first_day_currency_and_social.columns]
 
 # %%
-all_sales = df_sales.groupby('sku_hash').sum()['sales_quantity']
-all_sales = pd.DataFrame(all_sales)
-first_day_sales = first_day.groupby(['sku_hash', 'day_transaction_date', 'Month_transaction']).sum()['sales_quantity']
-first_day_sales = pd.DataFrame(first_day_sales)
-first_day_sales.columns = ['first_day_sales']
-first_day_sales.reset_index(inplace=True)
-first_day_sales.set_index('sku_hash', inplace=True)
+df_total_sales = df_sales.groupby('sku_hash').sum()['sales_quantity']
+df_total_sales = pd.DataFrame(df_total_sales)
+df_first_day_sales = df_first_day.groupby(['sku_hash', 'day_transaction_date', 'Month_transaction']).sum()['sales_quantity']
+df_first_day_sales = pd.DataFrame(df_first_day_sales)
+df_first_day_sales.columns = ['first_day_sales']
+df_first_day_sales.reset_index(inplace=True)
+df_first_day_sales.set_index('sku_hash', inplace=True)
 
 # %%
-sales_data = pd.merge(all_sales, first_day_sales, left_index=True, right_index=True)
-sales_data = pd.merge(sales_data, all_currency_and_social, left_index=True, right_index=True)
-sales_data = pd.merge(sales_data, first_day_currency_and_social, left_index=True, right_index=True)
+# Merge total with first day
+df_sales_data = pd.merge(df_total_sales, df_first_day_sales, left_index=True, right_index=True)
+logging.info("Merged df_total_sales {} df_first_day_sales {} = {}".format(df_total_sales.shape, df_first_day_sales.shape, df_sales_data.shape))
+
+# Merge all currency/social
+df_sales_data = pd.merge(df_sales_data, df_all_currency_and_social, left_index=True, right_index=True)
+logging.info("Merged df_all_currency_and_social {} = {}".format(df_sales_data.shape, df_sales_data.shape))
+
+# Merge first day curr/soc
+df_sales_data = pd.merge(df_sales_data, df_first_day_currency_and_social, left_index=True, right_index=True)
+logging.info("Merged df_first_day_currency_and_social {} {} = {}".format(df_first_day_currency_and_social.shape, df_first_day_sales.shape, df_sales_data.shape))
+log_df(df_sales_data,'df_sales_data')
 
 # %%
 monthDict = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
-sales_data.Month_transaction = sales_data.Month_transaction.astype('object').map(monthDict)
+df_sales_data.Month_transaction = df_sales_data.Month_transaction.astype('object').map(monthDict)
+logging.info("Mapped months in df_sales_data".format())
 
 # %%
-sales_data.head()
+logging.info("NAVIGATION DATA".format())
+df_first_day_navigation = df_navigation.loc[df_navigation.Date == 'Day 1', :]
+df_first_day_views = df_first_day_navigation.groupby('sku_hash').sum()[['page_views', 'addtocart']]
+df_first_day_views.columns = ['first_day_page_views', 'first_day_addtocart']
+df_views = df_navigation.groupby('sku_hash').sum()[['page_views', 'addtocart']]
+df_navigation_data = pd.merge(df_views, df_first_day_views, left_index=True, right_index=True)
+
 
 # %%
-first_day_navigation = navigation.loc[navigation.Date == 'Day 1',:]
-first_day_views = first_day_navigation.groupby('sku_hash').sum()[['page_views', 'addtocart']]
-first_day_views.columns = ['first_day_page_views', 'first_day_addtocart']
-views = navigation.groupby('sku_hash').sum()[['page_views', 'addtocart']]
-navigation_data = pd.merge(views, first_day_views, left_index=True, right_index=True)
+# Convert to float
+df_sales_data.sales_quantity = df_sales_data.sales_quantity.astype('float64')
+df_sales_data.first_day_sales = df_sales_data.first_day_sales.astype('float64')
 
 # %%
-sales_data.sales_quantity = sales_data.sales_quantity.astype('float64')
-sales_data.first_day_sales = sales_data.first_day_sales.astype('float64')
+# Log transform the sales
+df_sales_data['sales_quantity_log'] = (df_sales_data.sales_quantity + 1).apply(np.log)
+df_sales_data['first_day_sales_log'] = (df_sales_data.first_day_sales + 1).apply(np.log)
+
+
+#%% [markdown]
+# # Merge to main table!
+# %%
+
+df_all_merged = pd.merge(df_all, df_sales_data, left_on='sku_hash', right_index=True)
+df_all_merged = pd.merge(df_all_merged, df_navigation_data, how='left', left_on='sku_hash', right_index=True)
+df_all_merged = pd.merge(df_all_merged, df_vimages, left_on='sku_hash', right_on='sku_hash')
 
 # %%
-sales_data['sales_quantity_log'] = (sales_data.sales_quantity + 1).apply(np.log)
-sales_data['first_day_sales_log'] = (sales_data.first_day_sales + 1).apply(np.log)
-
-# %%
-train_data = pd.merge(train, sales_data, left_on='sku_hash', right_index=True)
-train_data = pd.merge(train_data, navigation_data, how='left', left_on='sku_hash', right_index=True)
-train_data = pd.merge(train_data, vimages, left_on='sku_hash', right_on='sku_hash')
-
-# %%
-test_data = pd.merge(test, sales_data, left_on='sku_hash', right_index=True)
-test_data = pd.merge(test_data, navigation_data, how='left', left_on='sku_hash', right_index=True)
-test_data = pd.merge(test_data, vimages, left_on='sku_hash', right_on='sku_hash')
-
-# %%
-train_data[navigation_data.columns] = train_data[navigation_data.columns].fillna(0)
-test_data[navigation_data.columns] = test_data[navigation_data.columns].fillna(0)
+df_all_merged[df_navigation_data.columns] = df_all_merged[df_navigation_data.columns].fillna(0)
+log_df(df_all_merged,'df_all_merged')
 
